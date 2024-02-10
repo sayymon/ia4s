cc42c722b40d7f185a2f1600e598420bf5d2079e
saymon.silva <saymon.silva@hotmart.com>
2024-02-10 14:33:59 -0300

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? '/ia4s' : '',
-}
+    output: 'export',
+};
+
+module.exports = nextConfig
 
 export default nextConfig;
