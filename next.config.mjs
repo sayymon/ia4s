18159fb96af29527428642d2149d798993ba3c28
saymon.silva <saymon.silva@hotmart.com>
2024-02-10 14:16:25 -0300

    Config pipeline gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    basePath: process.env.NODE_ENV === 'production' ? '/ia4s' : '',
+}
+
 export default nextConfig;
